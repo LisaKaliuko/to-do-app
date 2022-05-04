cf531921bad7e19b690351f94833d22784d709ca
Lisa Kaliuko <lisakaliuko@gmail.com>
2022-05-04 09:49:11 +0300

    Adding global styles and palette

---
+++ src/theme/index.tsx
@@ -0,0 +1,12 @@
+export const globalStyles = {
+  body: {
+    margin: '0px',
+    fontFamily: 'Verdana, sans-serif',
+  },
+};
+
+export const palette = {
+  $blackMain: '#000000',
+  $whiteMain: '#ffffff',
+  $captionText: '#808080',
+};
