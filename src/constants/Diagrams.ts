79c651bfda3b8efe06676b714507f885ab6895ce
Lisa Kaliuko <lisakaliuko@gmail.com>
2022-05-04 09:48:20 +0300

    Fixing configs, adding constants

---
+++ src/constants/Diagrams.ts
@@ -0,0 +1,33 @@
+export interface scaleDiagramProps {
+  name: string;
+  value: number;
+  color: string;
+}
+
+export const scaleDiagram: Array<scaleDiagramProps> = [
+  {
+    name: 'Statisctic 1',
+    value: 7,
+    color: '#333333',
+  },
+  {
+    name: 'Statisctic 2',
+    value: 3,
+    color: '#503f3f',
+  },
+  {
+    name: 'Statisctic 3',
+    value: 5,
+    color: '#6d4b4b',
+  },
+  {
+    name: 'Statisctic 4',
+    value: 1,
+    color: '#a86464',
+  },
+  {
+    name: 'Statisctic 5',
+    value: 8,
+    color: '#e27c7c',
+  },
+];
